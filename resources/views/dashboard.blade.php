8d65cbe935be6058844936d9ac71a2462c71b5df
jovelluna <jchrister718@gmail.com>
2023-06-16 17:47:11 +0800

    implemented basic layout for journals

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            Digital Journal
         </h2>
     </x-slot>
 
@@ -9,9 +9,43 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <span class="journal_title">Title Here</span><br><hr>
+
+                    hello <br>
+                    <button class="journal_edit_btn">Edit</button><button class="journal_delete_btn">Delete</button>
                 </div>
             </div>
+         
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <span class="journal_title">Title Here</span><br><hr>
+
+                    hello <br>
+                    <button class="journal_edit_btn">Edit</button><button class="journal_delete_btn">Delete</button>
+                </div>
+            </div>
+         
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <span class="journal_title">Title Here</span><br><hr>
+
+                    hello <br>
+                    <button class="journal_edit_btn">Edit</button><button class="journal_delete_btn">Delete</button>
+                </div>
+            </div>
+
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <span class="journal_title">Title Here</span><br><hr>
+
+                    hello <br>
+                    <button class="journal_edit_btn">Edit</button><button class="journal_delete_btn">Delete</button>
+                </div>
+            </div>
+         
+
+      
         </div>
     </div>
+    
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Journal
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" >
+                        Add New Journal
                     </x-nav-link>
                     
                 </div>
@@ -70,7 +73,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                Journal
             </x-responsive-nav-link>
         </div>
 
